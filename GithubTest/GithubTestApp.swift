dc72cc37c047a17bdc159faeaa02e4bb5b83bc20
Emerson Rouse <emersonmixrouse@gmail.com>
2023-08-11 22:47:27 -0600

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Emerson Rouse on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
